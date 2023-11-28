9099d11e4d1df03cdc6bf9a7449eef7e6acff65e
Simon-Briaux <simon.briaux2@gmail.com>
2023-11-28 11:58:42 +0100

    premier composant

---
+++ src/composant/testcolor.js
@@ -0,0 +1,21 @@
+import React, {useState } from 'react';
+
+const FunctionalComponent = ({ couleur }) => {
+    const [color, setColor] = useState(couleur);
+
+    const changeColor =() => {
+        const newColor = color === 'red' ? 'blue' : 'red';
+        setColor(newColor);
+    };
+
+    return (
+        <div>
+            <div style={{ color }}>
+                Je suis un composant
+            </div>
+            <button onClick={changeColor}>Changer la couleur</button>
+        </div>
+    )
+};
+
+export default FunctionalComponent;
