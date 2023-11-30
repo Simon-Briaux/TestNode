2bf260bbe73105ff22e405eacfa1879b0d738f08
Simon-Briaux <simon.briaux2@gmail.com>
2023-11-30 11:45:52 +0100

    composant afficher

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import FunctionalComponent from '.testcolor.js';
+import FunctionalComponent from './composant/testcolor';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
